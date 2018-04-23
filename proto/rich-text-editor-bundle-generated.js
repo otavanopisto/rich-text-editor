c6f75cd889d786ac15240f7cd61d3b0fde97ea30
anttiviljakainen <antti.viljakainen@otavanopisto.fi>
2018-04-23 21:04:08 +0300

    New generated version

--- proto/rich-text-editor-bundle-generated.js
+++ proto/rich-text-editor-bundle-generated.js
@@ -556,17 +556,16 @@ module.exports.makeRichText = (answer, options, onValueChanged = () => {}) => {
             if (!pasteInProgress) onValueChanged(u.sanitizeContent(e.currentTarget))
         })
         .on('drop', e => {
-//            setTimeout(() => {
-//                $(e.target).html(u.sanitize(e.target.innerHTML))
-//            },0)
-          if (e.originalEvent.dataTransfer) {
-            if (e.originalEvent.dataTransfer.files.length) {
-              console.log(e.originalEvent.dataTransfer.files[0]);
-              
-              var file = e.originalEvent.dataTransfer.files[0];
-              clipboard.onPasteFile(e, saver, onValueChanged, limit, file);
-            }
+          e.preventDefault();
+
+          if (e.originalEvent.dataTransfer && e.originalEvent.dataTransfer.files.length) {
+            var file = e.originalEvent.dataTransfer.files[0];
+            clipboard.onPasteFile(e, saver, onValueChanged, limit, file);
           }
+          
+          setTimeout(() => {
+            $(e.target).html(u.sanitize(e.target.innerHTML))
+          },0)
         })
         .on('paste', e => {
             pasteInProgress = true
@@ -616,7 +615,7 @@ module.exports = {
         'br'
     ],
     allowedAttributes: {
-        img: ['src', 'alt']
+        img: ['src', 'alt', 'class']
     },
     allowedSchemes: ['data'],
     exclusiveFilter: frame => frame.attribs['data-js'] === 'mathEditor'
